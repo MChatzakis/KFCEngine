952854619dfbdfe3851acdab5267169aa77ccef6
nikosfanou <68301728+nikosfanou@users.noreply.github.com>
2021-11-30 18:15:27 +0200

    maps and tiles

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 ## Installation Steps
 
+This project is made on Visual Studio 2019. In order to run it follow the instructions below :
+
+1) Git pull
+2) Open project as a visual studio solution (Select super-mario.sln file)
+3) Install allegro5 as NuGet package
+4) Build and Run
---
+++ super-mario/maps/characters.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="characters" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="646" columns="17">
+ <image source="../../../../../Desktop/csd/4o-etos/HY454/Project 2020/UnitTest3Media/characters.gif" width="300" height="650"/>
+</tileset>
