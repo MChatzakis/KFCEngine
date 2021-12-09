15cf67139d683093634eb194d8e02e4a4cb519ec
konto13 <nkontonasios@gmail.com>
2021-12-09 13:24:07 +0200

    Level1

--- super-mario/maps/super_mario_sprite_sheet.tsx
+++ super-mario/maps/super_mario_sprite_sheet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="super_mario_sprite_sheet" tilewidth="16" tileheight="16" tilecount="624" columns="26">
- <image source="../../../../../Desktop/csd/4o-etos/HY454/Project 2020/UnitTest3Media/super_mario_sprite_sheet.png" width="416" height="384"/>
+ <image source="../../UnitTest3Media/super_mario_sprite_sheet.png" width="416" height="384"/>
 </tileset>
--- super-mario/maps/super_mario_tiles.tsx
+++ super-mario/maps/super_mario_tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="super_mario_tiles" tilewidth="16" tileheight="16" tilecount="228" columns="19">
- <image source="../../../../../Desktop/csd/4o-etos/HY454/Project 2020/UnitTest3Media/super_mario_tiles.png" width="304" height="192"/>
+ <image source="../../UnitTest3Media/super_mario_tiles.png" width="304" height="192"/>
 </tileset>
