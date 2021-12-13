6201dbfb284e32e0ed4f41502d31e7de45020089
Manos Chatzakis <chatzakis@ics.forth.gr>
2021-12-13 18:15:15 +0200

    reInit of the repo

--- super-mario/maps/super_mario_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="super_mario_tiles" tilewidth="16" tileheight="16" tilecount="228" columns="19">
- <image source="../../UnitTest3Media/super_mario_tiles.png" width="304" height="192"/>
-</tileset>
