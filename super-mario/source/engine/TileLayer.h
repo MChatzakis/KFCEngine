4fe4d1495c2d20a5b4af936395497bba0b8b25fe
Manos Chatzakis <chatzakis@ics.forth.gr>
2021-12-31 01:40:32 +0200

    minor shit

--- super-mario/source/engine/TileLayer.h
+++ super-mario/source/engine/TileLayer.h
@@ -6,6 +6,68 @@
 #include "./General.h"
 #include "./GridLayer.h"
 
+void TileTerrainDisplay(TileMap* map, Bitmap dest, const Rect& viewWin, const Rect& displayArea, Bitmap* tiles) {
+	if (viewPosCached.x != viewWin.x || viewPosCached.y != viewWin.y) {
+		auto startCol = DIV_TILE_WIDTH(viewWin.x);
+		auto startRow = DIV_TILE_HEIGHT(viewWin.y);
+		auto endCol = DIV_TILE_WIDTH(viewWin.x + viewWin.w - 1);
+		auto endRow = DIV_TILE_HEIGHT(viewWin.y + viewWin.h - 1);
+		dpyX = MOD_TILE_WIDTH(viewWin.x);
+		dpyY = MOD_TILE_WIDTH(viewWin.y);
+		viewPosCached.x = viewWin.x, viewPosCached.y = viewWin.y;
+		std::cout << "Start col: " << startCol << " Start Row: " << startRow << " End col: " << endCol << " EndRow: " << endRow << std::endl;
+		for (Dim row = startRow; row <= endRow; ++row)
+			for (Dim col = startCol; col <= endCol; ++col)
+				PutTile(/*dpyBuffer*/ dest, MUL_TILE_WIDTH(col - startCol), MUL_TILE_HEIGHT(row - startRow), tiles, GetTile(map, col, row));
+	}
+}
+
+//!todo
+int GetMapPixelWidth(void) {
+	return TILEMAP_WIDTH * TILE_WIDTH; //change on define
+}
+
+//!todo
+int GetMapPixelHeight(void) {
+	return TILEMAP_HEIGHT * TILE_HEIGHT;
+}
+
+/*void Scroll(Rect* viewWin, int dx, int dy) {
+	viewWin->x += dx; viewWin->y += dy;
+}
+
+bool CanScrollHoriz(const Rect& viewWin, int dx) {
+	return viewWin.x >= -dx &&
+		(viewWin.x + viewWin.w + dx) <= GetMapPixelWidth();
+}*
+
+bool CanScrollVert(const Rect& viewWin, int dy) {
+	return viewWin.y >= -dy &&
+		(viewWin.y + viewWin.h + dy) <= GetMapPixelHeight();
+}*/
+
+static void FilterScrollDistance(int viewStartCoord, /* x or y*/int viewSize, /* w or h*/int* d, /* dx or dy*/ 	int maxMapSize /* w or h*/) {
+	auto val = *d + viewStartCoord;
+	if (val < 0)
+		*d = viewStartCoord; // cross low bound
+	else
+		if (viewSize >= maxMapSize)// fits entirely
+			*d = 0;
+		else
+			if ((val + viewSize) >= maxMapSize) // cross upper bound
+				*d = maxMapSize - (viewStartCoord + viewSize);
+}
+
+void FilterScroll(const Rect& viewWin, int* dx, int* dy) {
+	FilterScrollDistance(viewWin.x, viewWin.w, dx, GetMapPixelWidth());
+	FilterScrollDistance(viewWin.y, viewWin.h, dy, GetMapPixelHeight());
+}
+
+void ScrollWithBoundsCheck(Rect* viewWin, int dx, int dy) {
+	FilterScroll(*viewWin, &dx, &dy);
+	Scroll(viewWin, dx, dy);
+}
+
 class TileLayer {
 private:
 	GridLayer* grid = nullptr;
@@ -53,6 +115,8 @@ public:
 
 	void Display(Bitmap dest, const Rect& displayArea) {
 		assert(0);
+		al_draw_bitmap((ALLEGRO_BITMAP*)dest, displayArea.x, displayArea.y, 0);
+
 	}
 
 	Bitmap GetBitmap(void) const {
@@ -75,20 +139,22 @@ public:
 		return DIV_TILE_HEIGHT(viewWin.h);
 	}
 
+	//ok
 	void Scroll(float dx, float dy) {
-		assert(0);
+		viewWin.x += dx; 
+		viewWin.y += dy;
 	}
 
 	bool CanScrollHoriz(float dx) const {
-		assert(0);
+		return viewWin.x >= -dx && (viewWin.x + viewWin.w + dx) <= GetMapPixelWidth(); //?
 	}
 
 	bool CanScrollVert(float dy) const {
-		assert(0);
+		return viewWin.y >= -dy && (viewWin.y + viewWin.h + dy) <= GetMapPixelHeight();
 	}
 
 	auto ToString(void) const -> const std::string {
-		assert(0);
+		return std::string("To String is not implemented yet!");
 	}// unparse
 
 	bool FromString(const std::string&) {
@@ -111,15 +177,21 @@ public:
 
 	bool ReadText(FILE* fp) {
 		assert(0);
-	} // TODO: carefull generic parsing
+	}
 
+	//ok
 	TileLayer(Dim _rows, Dim _cols, Bitmap _tileSet) {
-		assert(0);
+		totalRows = _rows;
+		totalColumns = _cols;
+		tileSet =_tileSet;
+		
+		Allocate();
+
 	}
 
 	~TileLayer() {
-		assert(0);
-	}// cleanup here with care!
+		
+	}
 
 };
 
