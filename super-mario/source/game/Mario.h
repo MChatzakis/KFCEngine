f0dd10cc5c9680cf51b9d74e5de5edbc4fd29329
Manos Chatzakis <chatzakis@ics.forth.gr>
2022-01-17 17:42:56 +0200

    pushed json additions

---
+++ super-mario/source/game/Mario.h
@@ -0,0 +1,16 @@
+#ifndef _MARIO_H_
+#define _MARIO_H_
+
+
+#include "../engine/AnimationFilm.h"
+
+class Mario {
+
+private:
+	AnimationFilm* marioFilm;// = new AnimationFilm(BitmapLoad(""), );
+	//Sprite* marioSprite;
+
+	
+};
+
+#endif _MARIO_H_
