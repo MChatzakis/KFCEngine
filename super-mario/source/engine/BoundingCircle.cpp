346365c043acca35f0c98642eb0743e82a122be3
konto13 <nkontonasios@gmail.com>
2022-01-26 21:46:27 +0200

    update BoundingCircle.cpp

--- super-mario/source/engine/BoundingCircle.cpp
+++ super-mario/source/engine/BoundingCircle.cpp
@@ -17,7 +17,7 @@ bool BoundingCircle::In(unsigned _x, unsigned _y) const {
 	return pow(x - _x, 2) + pow(y - _y, 2) <= pow(r, 2);
 }
 
-bool BoundingCircle::Intersects(const BoundingBox& box) const { //todo
+/*bool BoundingCircle::Intersects(const BoundingBox& box) const { //todo
 	if (Point::pointDistance(Point(x, y), box.GetCenterPoint()) > r + box.GetDiagonal() / 2) {
 		return false;
 	}
@@ -26,6 +26,28 @@ bool BoundingCircle::Intersects(const BoundingBox& box) const { //todo
 	}
 
 	return true;
+}*/
+
+bool BoundingCircle::Intersects(const BoundingBox& box) const
+{
+	Point::Point p = box.GetCenterPoint();
+	int box_x = p.x;
+	int box_y = p.y;
+	int box_w = box.x2 - box.x1;
+	int box_h = box.y2 - box.y1;
+    int circleDistance_x = abs(this.x - box_x);
+    int circleDistance_y = abs(this.y - box_y);
+
+    if (circleDistance_x > (box_w/2 + this.r)) { return false; }
+    if (circleDistance_y > (box_h/2 + this.r)) { return false; }
+
+    if (circleDistance_x <= (box_w/2)) { return true; } 
+    if (circleDistance_y <= (box_h/2)) { return true; }
+
+    int cornerDistance_sq = pow((circleDistance_x - box_w/2),2) + 
+                         pow((circleDistance_y - box_h/2),2);
+
+    return (cornerDistance_sq <= pow(this.r,2));
 }
 
 bool BoundingCircle::Intersects(const BoundingPolygon& poly) const { //todo
